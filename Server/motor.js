2ac734750143b7e79210cb17cc42ce50ca068e9e
jku <jyrki.kujanpaa@gmail.com>
2016-03-31 13:12:21 +0300

    no console logs

--- Server/motor.js
+++ Server/motor.js
@@ -5,16 +5,15 @@ const yState = require('ysm/lib/yfsm').yState;
 
 
 var Motor = function() {
-
     const sm = yFsm();
-    const POSITION_TIMEOUT = 500;
-    var onPositionCB = null;
+    let positionTimeout = 50;
+    let onPositionCB = null;
     let state = 'no_state';
     let position = 0;
     let timeoutId;
 
     function signalPosition() {
-        console.log('signal position');
+        //console.log('signal position');
         sm.dispatch({signal: 'position'});
     };
 
@@ -22,11 +21,11 @@ var Motor = function() {
         handler(event) {
             switch (event.signal) {
                 case 'move_up':
-                    console.log('stopped - move_up');
+                    //console.log('stopped - move_up');
                     sm.transfer(running_up);
                     break;
                 case 'move_down':
-                    console.log('stopped - move_down');
+                    //console.log('stopped - move_down');
                     sm.transfer(running_down);
                     break;
                 default:
@@ -34,11 +33,11 @@ var Motor = function() {
             }
         },
         entry(event) {
-            console.log('Enter stopped.');
+            //console.log('Enter stopped');
             state = 'stopped';
         },
         exit(event) {
-            console.log('Exit stopped.');
+            //console.log('Exit stopped');
         }
     });
 
@@ -46,15 +45,15 @@ var Motor = function() {
         handler(event) {
             switch (event.signal) {
                 case 'stop':
-                    console.log('running_up - stop');
+                    //console.log('running_up - stop');
                     sm.transfer(stopped);
                 case 'position':
                     position += 1;
-                    console.log('running_up - position:' + position);
+                    //console.log('running_up - position:' + position);
                     onPositionCB(position);
-                    timeoutId = setTimeout(signalPosition, POSITION_TIMEOUT);
+                    timeoutId = setTimeout(signalPosition, positionTimeout);
                     if (position >= 10) {
-                        console.log('in top position');
+                        //console.log('in top position');
                         position = 10;
                         sm.transfer(stopped);
                     }
@@ -64,12 +63,12 @@ var Motor = function() {
             }
         },
         entry(event) {
-            console.log('Enter running_up.');
+            //console.log('Enter running_up');
             state = 'running_up';
-            timeoutId = setTimeout(signalPosition, POSITION_TIMEOUT);
+            timeoutId = setTimeout(signalPosition, positionTimeout);
         },
         exit(event) {
-            console.log('Exit running_up.');
+            //console.log('Exit running_up');
             clearTimeout(timeoutId);
         }
     });
@@ -78,14 +77,15 @@ var Motor = function() {
         handler(event) {
             switch (event.signal) {
                 case 'stop':
-                    console.log('running_down - stop');
+                    //console.log('running_down - stop');
                     sm.transfer(stopped);
                 case 'position':
                     position -= 1;
+                    //console.log('running_down - position:' + position);
                     onPositionCB(position);
-                    timeoutId = setTimeout(signalPosition, POSITION_TIMEOUT);
+                    timeoutId = setTimeout(signalPosition, positionTimeout);
                     if (position <= 0) {
-                        console.log('in bottom position');
+                        //console.log('in bottom position');
                         position = 0;
                         sm.transfer(stopped);
                     }
@@ -95,12 +95,12 @@ var Motor = function() {
             }
         },
         entry(event) {
-            console.log('Enter running_down.');
+            //console.log('Enter running_down');
             state = 'running_down';
-            timeoutId = setTimeout(signalPosition, POSITION_TIMEOUT);
+            timeoutId = setTimeout(signalPosition, positionTimeout);
         },
         exit(event) {
-            console.log('Exit running_down.');
+            //console.log('Exit running_down');
             clearTimeout(timeoutId);
         }
     });
@@ -112,6 +112,7 @@ var Motor = function() {
         onPosition: (cb) => {onPositionCB = cb},
         getState: () => state,
         getPosition: () => position,
+        setPosTimeout: (tm) => {positionTimeout = tm}
     });
 
 };
